29658e1916fe477f9ec7c00c86e9beffcad35531
Olaf Hering <olaf@aepfle.de>
2017-03-15 11:56:06 +0100

    auto configure
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+set -x
 autoreconf -vi
+env CFLAGS='-fmessage-length=0 -grecord-gcc-switches -fstack-protector -O2 -Wall -D_FORTIFY_SOURCE=2 -funwind-tables -fasynchronous-unwind-tables' \
+./configure --without-gcc-arch
