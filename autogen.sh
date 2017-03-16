19381ce185fdbfed89177920f945f97d49ca6d93
Olaf Hering <olaf@aepfle.de>
2017-03-16 13:07:56 +0100

    -grecord-gcc-switches
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- autogen.sh
+++ autogen.sh
@@ -2,7 +2,7 @@
 
 set -x
 autoreconf -vi
-env CFLAGS='-fmessage-length=0 -grecord-gcc-switches -fstack-protector -O2 -Wall -D_FORTIFY_SOURCE=2 -funwind-tables -fasynchronous-unwind-tables' \
+env CFLAGS='-fmessage-length=0 -fstack-protector -O2 -Wall -D_FORTIFY_SOURCE=2 -funwind-tables -fasynchronous-unwind-tables' \
 ./configure \
 	--without-mysql \
 	--without-gcc-arch
