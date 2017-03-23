f0355bd88ad49a328625b09a5afffca025a4e4ef
Olaf Hering <olaf@aepfle.de>
2017-03-23 16:23:41 +0100

    xxd
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- autogen.sh
+++ autogen.sh
@@ -7,6 +7,7 @@ automake --version
 pkg-config --version
 make --version
 gcc --version
+xxd --version
 autoreconf -vi
 env CFLAGS='-fmessage-length=0 -fstack-protector -O2 -Wall -D_FORTIFY_SOURCE=2 -funwind-tables -fasynchronous-unwind-tables' \
 ./configure \
