894ec8bb4680d98679fd4806ccf52583159ea9be
Olaf Hering <olaf@aepfle.de>
2017-07-10 12:21:04 +0200

    Only consider 1G/M/K mem.sh scripts
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- mem.sh
+++ mem.sh
@@ -4,7 +4,7 @@ if test -n "${tag}"
 then
   for cnt in {1..5}
   do
-    time for i in mem.*.sh ; do bash $i ; done
+    time for i in mem.1?.*.sh ; do bash $i ; done
     grep transferred log.memory.*.txt
     mkdir -vp "${tag}.${cnt}"
     mv -vit "$_" log.memory.*.txt
